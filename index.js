aa118c6ad3277e1b171522cc73d7feee5a57789d
ahmedysf100 <108271774+ahmeddysf100@users.noreply.github.com>
2022-08-03 17:31:31 +0300

    .

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-for (var i = 0; i <= 6; i++) {
+for (var i = 0; i <= 7; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", sound);
 
     function sound() {
@@ -22,18 +22,22 @@ for (var i = 0; i <= 6; i++) {
                 var tom1 = new Audio('sounds/tom-1.mp3');
                 tom1.play();
                 break;
+
             case "j":
                 var tom2 = new Audio('sounds/tom-2.mp3');
                 tom2.play();
                 break;
+
             case "k":
                 var tom3 = new Audio('sounds/tom-3.mp3');
                 tom3.play();
                 break;
+
             case "l":
                 var tom4 = new Audio('sounds/tom-4.mp3');
                 tom4.play();
                 break;
+
             case "he":
                 var fart = new Audio('sounds/fart.m4a');
                 fart.play();
